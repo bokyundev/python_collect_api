112cf19b947dd741fb9a55f13ae0bad919b01a24
bokyundev <bokyun.dev@gmail.com>
2020-06-30 17:38:39 +0900

    urllib 수정

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ url = 'http://apis.data.go.kr/B552061/trafficAccidentDeath/getRestTrafficAcciden
 # 파라미터
 param = urlencode({ 
     quote_plus('serviceKey') : "서비스키", 
-    quote_plus('searchYear') : '2014', 
-    quote_plus('siDo') : '1100', 
-    quote_plus('guGun') : '1117', 
+    quote_plus('searchYear') : '2018', 
+    quote_plus('siDo') : '2200', 
+    quote_plus('guGun') : '2208', 
     quote_plus('type') : 'xml', 
-    quote_plus('numOfRows') : '10', 
+    quote_plus('numOfRows') : '100', 
     quote_plus('pageNo') : '1' 
 })
 
 request = Request(url + unquote(param))
 request.get_method = lambda:'GET'
 response_body = urlopen(request).read()
+print(url + unquote(param))
 print(response_body)
